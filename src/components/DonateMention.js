18d1295d689b05d98d46e16c21b2150610fccf15
Nihey Takizawa <nihey.takizawa@gmail.com>
2020-11-06 13:00:36 -0300

    Make PayPal donation button more noticeable

--- src/components/DonateMention.js
+++ src/components/DonateMention.js
@@ -1,24 +1,47 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Button, Box } from '@material-ui/core'
 
 import { withTranslation } from '../../i18n'
 
 function DonateMention ({ t }) {
   return (
-    <Box mb={2} textAlign="center">
-      {t('Help us keep producing free web apps like these!')}
-      <div>
-        <a
-          href={`https://www.paypal.com/donate/?hosted_button_id=${t('R3TX8EDJ889SJ')}`}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {t('Click here to donate')}.
-        </a>
-        {' '}
-        {t('Every penny counts!')}
-      </div>
-    </Box>
+    <div className="donate-mention">
+      <Box mb={2} textAlign="center">
+        {t('Help us keep producing free web apps like these!')}
+        <div className="call-to-action">
+          <a
+            href={`https://www.paypal.com/donate/?hosted_button_id=${t('R3TX8EDJ889SJ')}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <Button variant="contained" color="primary">
+              <Box display="flex" alignItems="center">
+                {t('Donate with')}&nbsp;&nbsp;<img src="/paypal.png"/>
+              </Box>
+            </Button>
+          </a>
+        </div>
+      </Box>
+      <style jsx>{`
+        .donate-mention {
+          position: fixed;
+          bottom: 16px;
+          right: 16px;
+
+          @media (max-width: 1279px) {
+            position: unset;
+          }
+        }
+
+        .call-to-action {
+          padding-top: 16px;
+
+          img {
+            height: 24px;
+          }
+        }
+      `}</style>
+    </div>
   )
 }
 
