dca6ce5103b7dc927b9aeae9f4068f8e06d36806
Nihey Takizawa <nihey.takizawa@gmail.com>
2020-11-17 15:51:46 -0300

    Add PAYPAL_ID environment variable to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
+  env: {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    PAYPAL_ID: process.env.PAYPAL_ID
+  },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
