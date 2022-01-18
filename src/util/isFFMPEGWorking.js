b62c64e27bcc55e681c639b36bc09d8c4276f396
Nihey Takizawa <nihey.takizawa@gmail.com>
2022-01-18 11:31:30 -0300

    Disable local ffmpeg for now

--- src/util/isFFMPEGWorking.js
+++ src/util/isFFMPEGWorking.js
@@ -1,31 +1,36 @@
-import ffmpeg from './ffmpeg'
+// import ffmpeg from './ffmpeg'
 
 export default async function isFFMPEGWorking () {
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-  if (isSafari) {
-    return false
-  }
-
-  let isResolved = false
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (!isResolved) {
-        isResolved = true
-        resolve(false)
-      }
-    }, 3000)
-
-    ffmpeg({
-      MEMFS: [],
-      arguments: [
-        '-version'
-      ],
-      onDone: (data) => {
-        if (!isResolved) {
-          isResolved = true
-          resolve(true)
-        }
-      }
-    })
-  })
+  return false
+  // Disable local ffmpeg as it cannot render large resolution videos depending
+  // on the person's device. Rendering the animation on the server will give
+  // more consistent results. We can do some more tests with it later.
+  //
+  // const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+  // if (isSafari) {
+  //   return false
+  // }
+  //
+  // let isResolved = false
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     if (!isResolved) {
+  //       isResolved = true
+  //       resolve(false)
+  //     }
+  //   }, 3000)
+  //
+  //   ffmpeg({
+  //     MEMFS: [],
+  //     arguments: [
+  //       '-version'
+  //     ],
+  //     onDone: (data) => {
+  //       if (!isResolved) {
+  //         isResolved = true
+  //         resolve(true)
+  //       }
+  //     }
+  //   })
+  // })
 }
